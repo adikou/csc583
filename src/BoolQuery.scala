b87acc932c9f0a50d0445a84922b64e0dcd304ec
Aditya Kousik <adit267.kousik@gmail.com>
2015-09-17 16:33:08 -0700

    Modified AND, OR operators to disambiguate b/w words 'and', 'or'; Error checking

--- src/BoolQuery.scala
+++ src/BoolQuery.scala
@@ -5,6 +5,7 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.Queue
 import scala.collection.mutable.Stack
 import scala.io.Source
+import java.nio.file.{Files, FileSystems}
 
 /**
  * @author adikou
@@ -18,14 +19,19 @@ object BoolQuery {
      */
   
     private var index = HashMap.empty[String, ListBuffer[Int]]
-    private val cmds = Array("build", "query", "help")
-    private val ops = Array("AND", "OR")
-    private val opPrecedence = Predef.Map("AND" -> 2, "OR" -> 1)
-    private val opFuncMap = Predef.Map("AND" -> {this.AND(_,_)},
-                                       "OR"  -> {this.OR (_,_)})
+    private val cmds = Array("build", "query", "help", "quit")
+    private val ops = Array("\\AND", "\\OR")
+    private val opPrecedence = Predef.Map("\\AND" -> 2, "\\OR" -> 1)
+    private val opFuncMap = Predef.Map("\\AND" -> {this.AND(_,_)},
+                                       "\\OR"  -> {this.OR (_,_)})
     private val err = """|Unrecognized command. Type 'help' for a list
                          |of commands""".stripMargin.replaceAll("\n", " ")
-    private val cmd_help = "\nbuild <DOCUMENT>\nquery <QUERY>\n"
+    private val cmd_help =  "\nbuild <path/to/DOCUMENT>\n"
+                           .concat("query <QUERY> [note: Use \\and,") 
+                           .concat(" \\or for logical ")
+                           .concat("operators]\n")
+                           .concat("quit\n")
+      
     
     def map(words: ArrayBuffer[Posting]) = {
         val terms = HashMap.empty[String, ListBuffer[Int]]
@@ -56,10 +62,24 @@ object BoolQuery {
     }
     
     def constructInvertedIndex(file: String) = {
-        val lines = Source.fromFile(file).getLines.toList
-        val words = BoolQuery.tokenize(lines)
-        CustomMergeSort.mergesort(words, 0, words.length-1)
-        this.index = BoolQuery.map(words)
+        var flag = true
+        if(Files.isDirectory(FileSystems.getDefault().getPath(file))) {
+            println(file + " is a directory. Enter valid filepath")
+            flag = false
+        }
+        else if(!Files.exists(FileSystems.getDefault().getPath(file))) {
+            println(file + " does not exist. Enter valid filename")
+            flag = false
+        }
+        
+        if(flag) {
+            val lines = Source.fromFile(file).getLines.toList
+            val words = BoolQuery.tokenize(lines)
+            CustomMergeSort.mergesort(words, 0, words.length-1)
+            this.index = BoolQuery.map(words)
+        }
+        
+        flag
     }
     
     
@@ -107,7 +127,7 @@ object BoolQuery {
      * convert the infix query to RPN notation
      * */
     
-    def parseQuery(query: Array[String]) = {
+    def parseQuery(query: Array[String]): Array[String] = {
         val rpnQuery = new Queue[String]
         val opStack  = new Stack[String]
         for(token <- query) {
@@ -123,8 +143,11 @@ object BoolQuery {
             else if(token.equals(")")) {
                 while(opStack.length > 0 && !opStack.top.equals("("))
                     rpnQuery.enqueue(opStack.pop)
-                if(opStack.length == 0)
-                    "err"
+                if(opStack.length == 0){
+                    println("Syntax error - missing (")
+                    rpnQuery.clear()
+                    return rpnQuery.toArray
+                }
                 opStack.pop                    
             }
             else rpnQuery.enqueue(token)
@@ -132,7 +155,7 @@ object BoolQuery {
         
         while(opStack.length > 0)
             rpnQuery.enqueue(opStack.pop)
-        
+            
         rpnQuery.toArray
     }
     
@@ -140,18 +163,25 @@ object BoolQuery {
      * Stack based evaluation of RPN notation
      * */
     
-    def evaluateQuery(query: Array[String]) = {
+    def evaluateQuery(query: Array[String]): Unit = {
         val op = new Stack[List[Int]]
-        for(token <- query) {
-            if(ops.contains(token.toUpperCase)) {
-                if(op.length > 0)
-                    op.push(opFuncMap(token.toUpperCase)(
-                            op.pop, op.pop))
-            }
-            else op.push(index(token).toList)
+        if(query.length > 0) {
+          for(token <- query) {
+              if(ops.contains(token.toUpperCase)) {
+                  if(op.length > 0)
+                      op.push(opFuncMap(token.toUpperCase)(
+                              op.pop, op.pop))
+              }
+              else if(index.contains(token))
+                  op.push(index(token).toList)
+              else {
+                  println("Error: " + token + ": no such term/operator")
+                  return
+              }
+          }
+          if(op.top.length == 0) println("NIL")
+              else println(op.top.mkString(" -> "))
         }
-        println(op.top.mkString(" -> "))
-        op.pop
     }
     
     def main(args: Array[String]) = {
@@ -167,15 +197,18 @@ object BoolQuery {
             val cmdArgs = cmdLine.replaceAll("[(]", "( ")
                                  .replaceAll("[)]", " )").split(" ").toArray
             cmds.indexOf(cmdArgs(0)) match {
-              case 0 => BoolQuery.constructInvertedIndex(cmdArgs(1))
-                        println("Inverted index constructed for " + cmdArgs(1))
-              case 1 => BoolQuery.evaluateQuery(
+              case 0 => if(BoolQuery.constructInvertedIndex(cmdArgs(1)))
+                            println("Inverted index constructed for " 
+                                    + cmdArgs(1))
+              case 1 => 
+                        BoolQuery.evaluateQuery(
                           (BoolQuery.parseQuery(
                                        cmdArgs.slice(1, cmdArgs.length+1)
                                      )
                           )
                         )
               case 2 => println (cmd_help)
+              case 3 => System.exit(0)
               case _ => println (err)
             } 
             
