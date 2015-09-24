ccbcb91ec7fa7bd33840e86eb18e078c871ce243
Aditya Kousik <adit267.kousik@gmail.com>
2015-09-24 13:28:43 -0700

    Fixed change of 'c' to 'd'

--- src/Helper.scala
+++ src/Helper.scala
@@ -1,4 +1,4 @@
 object Helper {
-	def splitOp(x: String) = x.replace("c", " c ").replace("u", " u ").split(" ")
+	def splitOp(x: String) = x.replace("d", " d ").replace("u", " u ").split(" ")
   	def processOp(x: String) = x.replaceAll("[0-9]","").toLowerCase
 }
